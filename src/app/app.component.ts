53734e7c397e60bed8fbb512c2477c9d0077ef6d
Minh Nghia <nguyenminhnghia.thd@gmail.com>
2023-07-03 13:20:23 +0700

    add example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { LoggerService } from './core/services/logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AutoDraw';
+  title: string = 'AutoDraw';
+  fontColor: string = 'red';
+  sayHelloId: string = 'sayHelloId';
+
+  canClick: boolean = true;
+  messenger: string = "I'm read only!";
+  count: number = 0;
+
+  constructor(private logger: LoggerService) {}
+
+  toggleClick = () => {
+    this.canClick = !this.canClick;
+    if (this.canClick) {
+      this.messenger = 'You can edit me!';
+    } else {
+      this.messenger = "I'm read only!";
+    }
+  };
+
+  showAlert = () => {};
+
+  onLogMe() {
+    this.logger.writeCount(this.count);
+    this.count++;
+  }
 }
